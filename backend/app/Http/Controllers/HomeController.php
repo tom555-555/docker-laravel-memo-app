5e28901a505e9b984c64cb74a63a9326a5390e6f
Tomoki_Kaneko <Tomoki_Kaneko@T-MacBook-Pro.local>
2021-11-03 00:53:06 +0900

    メモコンテンツ：データ参照 / 追加

---
+++ backend/app/Http/Controllers/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Memo;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $memos= Memo::Select('memos.*')
+            ->where('user_id', '=', \Auth::id())
+            ->whereNull('deleted_at')
+            ->orderBy('updated_at', 'DESC')
+            ->get();
+
+        return view('create', compact('memos'));
+    }
+
+    public function store(Request $request)
+    {
+        $posts = $request->all();
+        Memo::insert(['content' => $posts['content'], 'user_id' => \Auth::id() ]);
+
+        return redirect( route('home'));
+    }
+}
